fcd1fd14a4555c031bd687263347fd0a174af698
ericong18 <ericong18@gmail.com>
2018-08-05 15:57:54 -0700

    Created feedback messages specific to user's rating

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,6 +6,12 @@ let trWhiteBlock = require('../assets/imgs/tr white block.png');
 
 class Results extends Component {
 
+  state = {
+    stars: 0,
+    review: '',
+    rating: undefined
+  }
+
   normalize(rating) {
     return Number(((rating + 1) / 2) * 5).toFixed(2);
   }
@@ -13,6 +19,29 @@ class Results extends Component {
   componentDidMount = () => {
     console.log("updating prop with " + this.props.rating);
     this.props.update(this.props.rating);
+    this.setState({
+      stars: this.props.stars,
+      review: this.props.review,
+      rating: this.normalize(this.props.rating)
+    })
+  }
+
+  feedback = () => {
+    console.log("feedback");
+    let userRating = this.state.stars;
+    console.log(userRating);
+    let truRating = this.state.rating;
+    console.log(truRating);
+
+    if (Math.abs(userRating - truRating) <= 0.5) {
+      return "Nice! We pretty much agree with your rating too.";
+    }
+    else if (userRating - truRating > 0.5) {
+      return "You seem to be a bit too generous. Try reflecting more in your words or consider lowering that rating...";
+    }
+    else {
+      return "You might be a bit too harsh. You might want to consider increasing your rating or reflecting more in your words...";
+    }
   }
 
   render() {
@@ -22,8 +51,10 @@ class Results extends Component {
         <div className="row justify-content-center">
           <div className="col-md-8 info-container">
             <div className="row justify-content-center score-container">
-              <p id="tr-score">Tru Review score: <span id="score">{ this.normalize(this.props.rating) } &#9733;</span></p>
-              <p id="feedback"></p>
+              <p id="tr-score">Tru Review rating: <span id="score">{ this.state.rating } &#9733;</span></p>
+            </div>
+            <div className="row justify-content-center feedback-container">
+              <p id="feedback">{ this.feedback() }</p>
             </div>
           </div>
         </div>
